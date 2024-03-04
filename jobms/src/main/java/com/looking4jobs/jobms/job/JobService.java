8d98c85c36203eb4953595bfa66cd62ae20108b0
Gabriel Soares <gabrielalcantarars@gmail.com>
2024-03-04 00:21:20 -0300

    Job ms basics and postgresql config

---
+++ jobms/src/main/java/com/looking4jobs/jobms/job/JobService.java
@@ -0,0 +1,17 @@
+package com.looking4jobs.jobms.job;
+
+import java.util.List;
+
+public interface JobService {
+
+    List<Job> findAll();
+
+    void createJob(Job job);
+
+    Job getJobById(Long id);
+
+    boolean deleteJobById(Long id);
+
+    boolean updateJob(Long id, Job updatedJob);
+
+}
